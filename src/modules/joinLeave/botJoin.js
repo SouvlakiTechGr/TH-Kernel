93f873860ddd710252a242770440737303cafe8c
SouvlakiTechGr <73787013+SouvlakiTechGr@users.noreply.github.com>
2022-06-10 18:06:30 +0300

    Update botJoin.js

--- src/modules/joinLeave/botJoin.js
+++ src/modules/joinLeave/botJoin.js
@@ -1,3 +1,4 @@
+const { MessageActionRow, MessageButton } = require('discord.js');
 module.exports = {
     async execute(client) {
         client.on("guildCreate", guild => {
@@ -12,9 +13,19 @@ module.exports = {
             })
                 const newserverembed = new Discord.MessageEmbed()
                   .setDescription('Hello! Thanks for adding me to your server! My name is **' + Distro + '** . \nStart by configuring me to better suit your server like telling me the modlog channels! To do that, run `*setup` . My prefix is `*` . \nCurrently the bot has only a few commands, but don\'t worry! There will soon be more. \nTo give suggestions for the bot, run `*suggestion [suggestion]` and the suggestion will be sent to the bot owners. \nCheck out our currency system too! It\'s currently simple, but don\'t worry! More will be added.')
-                  .addFields(
-                    { name: 'Links', value: 'Support Server: https://discord.gg/Mdyjzvf \nInvite: <https://discordapp.com/oauth2/authorize?client_id=714001186897788934&scope=bot&permissions=2146958847>'}
-                  )
+            const row = new MessageActionRow()
+                .addComponents(
+                    new MessageButton()
+                        .setLabel('Support Server')
+                        .setStyle('LINK')
+                        .setURL('')
+            )
+                .addComponents(
+                    new MessageButton()
+                        .setLabel('Invite')
+                        .setStyle('LINK')
+                        .setURL('')
+            )
                 defaultChannel.send(newserverembed);
                 client.user.setPresence({ activity: { name: `in `+client.guilds.cache.size+ ` servers` }, type: 'WATCHING', status: 'idle' })
             })
